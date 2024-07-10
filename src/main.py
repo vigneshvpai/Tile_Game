7db4eb7ce8aa2e8ef78e0f40113cc084764e2c9c
Vignesh V Pai <vigneshvpai@gmail.com>
2024-07-10 13:54:12 +0200

    Submission

--- src/main.py
+++ src/main.py
@@ -12,22 +12,28 @@ if __name__ == "__main__":
     matrix_and_colors.set_colors()
     matrix_and_colors.set_matrix()
 
-    ans = input("Do you want to play? (Y/N): ")
+    computer = Computer.Computer(matrix_size=matrix_size, no_of_colors=no_of_colors)
+    computer.play_game()
 
-    if ans == "Y":
-        matrix_and_colors.display()
-        while matrix_and_colors.new_tiles_count < np.square(
-            matrix_and_colors.matrix_size
-        ):
-            next_color = int(input("Enter next color: "))
-            print(
-                "New tiles: ",
-                matrix_and_colors.flood_fill((0, 0), next_color, modify_matrix=True),
-            )
-            matrix_and_colors.display()
-    else:
-        computer = Computer.Computer(matrix_size=matrix_size, no_of_colors=no_of_colors)
-        computer.play_game()
+    print()
+    print("Best move: ", computer.moves[:-1])
 
-        print()
-        print("Best move: ", computer.moves[:-1])
+    # ans = input("Do you want to play? (Y/N): ")
+
+    # if ans == "Y":
+    #     matrix_and_colors.display()
+    #     while matrix_and_colors.new_tiles_count < np.square(
+    #         matrix_and_colors.matrix_size
+    #     ):
+    #         next_color = int(input("Enter next color: "))
+    #         print(
+    #             "New tiles: ",
+    #             matrix_and_colors.flood_fill((0, 0), next_color, modify_matrix=True),
+    #         )
+    #         matrix_and_colors.display()
+    # else:
+    #     computer = Computer.Computer(matrix_size=matrix_size, no_of_colors=no_of_colors)
+    #     computer.play_game()
+
+    #     print()
+    #     print("Best move: ", computer.moves[:-1])
